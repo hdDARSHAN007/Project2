1dafcdce789d0eade29a3bdcaad9e56b1631e9e4
DARSHAN HD <hdd78559@gmail.com>
2025-03-16 09:55:39 +0530

    added New Feature Main

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-let document = document.querySelector('btn');
+let box = document.querySelector('button');
+
+box.addEventListener("dblclick",()=>{
+    console.log("Double clicked btn");
+    console.log("please click once!!!");
+})
